0b1f59d7a23eaee7310dcaefd8586c38a18355d1
LasseStaus <l.stausgaard@gmail.com>
2025-06-05 11:11:12 +0200

    basepath & Prefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/karma-screen-reload',
+  assetPrefix: '/karma-screen-reload',
 
   images: {
     remotePatterns: [
